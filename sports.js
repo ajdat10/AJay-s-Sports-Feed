5abd1757d7f41a8cf48bab264f6c025780271857
Andrew Cruz <andrewcruz@Andrews-MacBook-Air.local>
2020-10-21 19:31:45 -0400

    added more HTMLs

--- sports.js
+++ sports.js
@@ -1,28 +1,47 @@
 
-const SPORTS_LIST_URL = `https://www.thesportsdb.com/api/v1/json/1/all_sports.php`
+const SPORTS_LIST_URL = `https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=4391`
 
-const getSportsLeague = async () =>{
+const getNFL = async () =>{
     try{ 
         const response = await axios.get(SPORTS_LIST_URL)
-        buildDropdown(response.data)
-    //  console.log(response.data) //IT WORKS!!!! Display an array of sports 
+        displayNFL(response.data)
+        //  console.log(response.data) //IT WORKS!!!! Display an array of sports 
     }catch(error){
         console.log(error)
     }
 }
-    const buildDropdown = (data) => {
-        const dropdownDiv = document.querySelector('.dropdown')
-        const dropdown = document.createElement('select')
-        // console.log(typeof data)
-        data.sports.forEach(sports => {
-            console.log(sports)
-            let optionElement = document.createElement('option')
-            optionElement.innerText = `${sports.strSport}`
-            optionElement.setAttribute('value', sports.strSport)
-            dropdown.appendChild(optionElement)
-        })
-        dropdownDiv.appendChild(dropdown)
-    }
+const displayNFL = (data) =>{
+    
+    const displayDiv = document.querySelector('.myData')
+    for (let i = 0; i< data.leagues.length; i++){
+        
+        console.log(data.leagues[i].strSport)
+    const nflDiv = document.createElement('div')
+    nflDiv.innerHTML = `${data.leagues[i].strSport}`
+   
+    
     
 
-window.onload = getSportsLeague
+   displayDiv.appendChild(nflDiv)
+    }
+// getNFL()
+}
+
+window.onload = getNFL()
+   
+//     const buildDropdown = (data) => {
+//         const dropdownDiv = document.querySelector('.dropdown')
+//         const dropdown = document.createElement('select')
+//         // console.log(typeof data)
+//         data.sports.forEach(sports => {
+//             console.log(sports)
+//             let optionElement = document.createElement('option')
+//             optionElement.innerText = `${sports.strSport}`
+//             optionElement.setAttribute('value', sports.strSport)
+//             dropdown.appendChild(optionElement)
+//         })
+//         dropdownDiv.appendChild(dropdown)
+    // }
+    
+
+// window.onload = getSportsLeague
